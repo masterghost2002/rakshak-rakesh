facd617b39eb98ac6c77d95206b84501cc0bf045
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-02-06 16:46:54 +0530

    masterghost2002:added header in public layout

--- web/src/layouts/public.layout.tsx
+++ web/src/layouts/public.layout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
-import { Container } from "@mui/material";
+import { Container, AppBar, Toolbar, Typography } from "@mui/material";
 import { Toaster } from "react-hot-toast";
 const PublicLayout = () => {
     return (
-        <Container maxWidth='xl'>
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Typography variant="h6" color="inherit" component="div">
+                        Rakshak
+                    </Typography>
+                </Toolbar>
+            </AppBar>
             <Toaster />
-            <Outlet/>
-        </Container>
+            <Container  style = {{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                <Outlet />
+            </Container>
+        </>
     );
 };
 export default PublicLayout;
